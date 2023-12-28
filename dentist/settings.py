128f12756a7e9c38f2756d3c0520e5af06548bed
soetje4 <153307106+soetje4@users.noreply.github.com>
2023-12-28 09:53:17 +0100

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mw^pws+2dkm4dk7&)fer@eh--j1kw$e(zl-9+^qg6e)n%**dch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
